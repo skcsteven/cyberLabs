8f46d459d402ea3308268db2472eea7960c1bffe
Steve Chen <65736346+skcsteven@users.noreply.github.com>
2023-02-18 20:43:40 -0500

    Create firstBashEver.sh

---
+++ overthewire/krypton/krypton2/firstBashEver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+start = "OMQEMDUEQMEK"
+for ((i = 0; i < 26; i++)); do
+        /krypton/krypton2/encrypt text.txt
+        cat ciphertext
+         > text.txt
+        cat ciphertext >> text.txt
+done
