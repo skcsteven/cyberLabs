1cf9f6c08517f1cf4f8a50fb743139b11d78407c
Steve Chen <65736346+skcsteven@users.noreply.github.com>
2023-02-18 21:06:36 -0500

    Update firstBashEver.sh

--- overthewire/krypton/krypton2/firstBashEver.sh
+++ overthewire/krypton/krypton2/firstBashEver.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-start = "OMQEMDUEQMEK"
+echo "OMQEMDUEQMEK" > text.txt
 for ((i = 0; i < 26; i++)); do
         /krypton/krypton2/encrypt text.txt
         cat ciphertext
+        echo "   "
          > text.txt
         cat ciphertext >> text.txt
 done
